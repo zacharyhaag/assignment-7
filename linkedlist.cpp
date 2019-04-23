45aacc8f3995d794f0053a8afae52dbed52ec295
zacharyhaag <32313367+zacharyhaag@users.noreply.github.com>
2019-04-23 10:04:54 -0700

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,33 @@
+#include "linkedlist.h"
+
+linkedlist::linkedlist() {
+
+}
+
+linkedlist::~linkedlist() {
+    // RECURSIVE
+}
+
+int linkedlist::count() {
+    // RECURSIVE
+}
+
+void linkedlist::addToFront(int n) {
+
+}
+
+double linkedlist::average() {
+    // uses sum and count functions
+}
+
+int linkedlist::sum() {
+    // RECURSIVE
+}
+
+void linkedlist::writeInorder(string & file) {
+
+}
+
+void linkedlist::writeReversed(string & file) {
+    // RECURSIVE
+}
